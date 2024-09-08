fbf6bc0425d676c545ee006cc453c6da61098f3b
leochmoc <leochmoc@gmail.com>
2024-09-08 13:24:02 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-9xl animate-shine bg-clip-text font-mono font-bold  text-transparent bg-gradient-to-r from-blue-950 via-sky-300 to-blue-950">
+      React Labs
+    </h1>
+  )
+}
+
+export default App
